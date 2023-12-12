4a843774e6e446a88b7c6b53653c1db51385501c
cacapgui02 <guilherme.16.cap@gmail.com>
2023-12-12 15:39:36 -0300

    Initial commit

---
+++ ex08/.js
@@ -0,0 +1,6 @@
+var vel = 50
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) { //CONDIÇÃO SIMPLES
+    console.log(`Voce ultrapassou a velocidade permitida. MULTADO`)
+}
+console.log('Dirija sempre com sinto de segurança') 
